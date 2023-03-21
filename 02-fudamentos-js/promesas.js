6b6d6952114646b25087aea5b36aeb36b3409622
Maximiliano Andres Zurita Mañan <maximiliano.zurita@gamaleasing.cl>
2023-03-21 16:44:15 -0300

    21-03-23

--- 02-fudamentos-js/promesas.js
+++ 02-fudamentos-js/promesas.js
@@ -29,17 +29,54 @@ const salarios = [
 ]
 
 const getEmpleado = (id) =>{
-    const promesa = new Promise((resolve , reject)=>{
-        const empleado = empleados.find((e) => e.id === id)?.nombre
+    return promesa = new Promise((resolve , reject)=>{
+        const empleado = empleados.find(e => e.id === id)?.nombre;
 
-        if (empleado) {
-            resolve(empleado);
-        }else{
-            reject(`no existe empleado con el id ${id}`);
-        }
+        // if (empleado) {
+        //     resolve(empleado);
+        // }else{
+        //     reject(`no existe empleado con el id ${id}`);
+        // }
 
+        (empleado)?resolve(empleado) 
+        :reject(`no existe empleado con el id ${id}`);
     });
-    return promesa;
+    
 }
-const id = 1;
-getEmpleado(id).then(empleado => console.log(empleado));
+
+const getSalario = (id) =>{
+    return promesa = new Promise((resolve , reject)=>{
+        const salario = salarios.find(s => s.id === id)?.salarios;
+
+        // if (empleado) {
+        //     resolve(empleado);
+        // }else{
+        //     reject(`no existe empleado con el id ${id}`);
+        // }
+
+        (salario)?resolve(salario) 
+        :reject(`no existe salario con el id ${id}`);
+    });
+    
+}
+
+const id = 4;
+
+    // getEmpleado(id)
+    // .then(empleado => console.log(empleado))
+    // .catch(err => console.log(err));
+
+    // getSalario(id)
+    // .then(salario => console.log(salario))
+    // .catch(err => console.log(err));
+
+getEmpleado(id)
+    .then(empleado => {
+        getSalario(id)
+        .then(salario =>{
+            console.log('El empleado' , empleado , 'Tiene una salario' , salario);
+        })
+    })
+    .catch(err => console.log(err))
+
+
